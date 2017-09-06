70c1bc2139ab669719c925451f38860b1bec3552
RIFAIQBAL <31480663+RIFAIQBAL@users.noreply.github.com>
2017-09-06 21:38:30 +0530

    Add files via upload

---
+++ 4.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+int a;
+cout<<"enter your number";
+cin>>a;
+if(a%5==0&&a%11==0)
+cout<<"your number is divisible by both 5 and 11";
+else
+cout<<"your number is not divisible by 5 or 11";
+}
