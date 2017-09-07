5272baedf6d27fca71047aadece2aa11e8191164
RIFAIQBAL <31480663+RIFAIQBAL@users.noreply.github.com>
2017-09-07 14:54:34 +0530

    Add files via upload

---
+++ 11.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+using namespace std;
+int main(){
+int a;
+cout<<"enter the weakday";
+cin>>a;
+if(a==1)
+cout<<"the day is monday";
+else if(a==2)
+cout<<"the day is tuesday";
+else if(a==3)
+cout<<"the day is wednesday";
+else if(a==4)
+cout<<"the day is thursday";
+else if(a==5)
+cout<<"the day is friday";
+else if(a==6)
+cout<<"the day is saturday";
+else if(a==7)
+cout<<"the day is sunday";
+else
+cout<<"invalid input";
+}
