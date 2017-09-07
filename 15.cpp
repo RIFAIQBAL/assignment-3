ff55d8f41d1e3ad41de28dad1bbfbc26042652e9
RIFAIQBAL <31480663+RIFAIQBAL@users.noreply.github.com>
2017-09-07 18:02:20 +0530

    Add files via upload

--- 15.cpp
+++ 15.cpp
@@ -2,13 +2,11 @@
 using namespace std;
 int main()
 {
-int a,b,c;
-cout<<"enter the three sides of a triangle :"<<endl;
+float a,b,c;
+cout<<"enter the three sides of a triangle"<<endl;
 cin>>a>>b>>c;
-if((a==b)&&(b==c))
-cout<<"the triangle is equlateral";
-else if((a==b)&&(a!=c)||(a==c)&&(a!=b)||(b==c&&c!=a))
-cout<<"the triangle is isosceles";
+if((a+b>c)&&(a+c>b)&&(b+c>a))
+cout<<"the triangle is valid";
 else
-cout<<"the triangle is scalene";
+cout<<"the triangle is not valid";
 }
